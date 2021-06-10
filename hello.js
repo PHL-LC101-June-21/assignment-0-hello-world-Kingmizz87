737b96fa69c7824a1a4e84ce39bc3addb69174ff
King White <85591126+Kingmizz87@users.noreply.github.com>
2021-06-10 03:38:45 +0000

    changed it again

--- hello.js
+++ hello.js
@@ -1,7 +1,8 @@
+
 function hello() {
-	console.log("Hello, World!"); 
+	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
-	return "Hello!";
+	return "Hello World!";
 }
  
 module.exports = hello;
